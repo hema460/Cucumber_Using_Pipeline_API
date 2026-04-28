22dbc41ec4ae907c427da1501b993d99ba9ed265
hema <hemajayanthreddy@gmail.com>
2026-04-28 12:39:35 -0500

    added runner

--- src/test/java/testRunner/TestRunner.java
+++ src/test/java/testRunner/TestRunner.java
@@ -4,15 +4,17 @@ import org.testng.annotations.DataProvider;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
-@CucumberOptions(features="src/test/resources/features", glue={"stepDef"},plugin={"pretty","html:target/cucumber-html-report",               // optional basic report
-"json:target/cucumber-report/cucumber.json"}, // required for Masterthought
+@CucumberOptions(features="src/test/resources/features", glue={"stepDef"},plugin = {
+	    "pretty",
+	    "json:target/cucumber-report/cucumber.json"
+	}, // required for Masterthought
 monochrome=true,dryRun=false)
 public class TestRunner extends AbstractTestNGCucumberTests{
 
 	@DataProvider(parallel=false)
 	public Object[][] scenarios() {
 		
-		System.setProperty("cucumber.features.rertry", "1");
+		System.setProperty("cucumber.features.retry", "1");
 		return super.scenarios();
         
     }
